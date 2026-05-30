5960630f73608e23d7f64139cd493eef9a95d627
Adam White <whitea0197@gmail.com>
2026-05-30 21:42:35 +1000

    Difftool

--- git.sh
+++ git.sh
@@ -2,3 +2,5 @@
 
 touch ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
+git config --global diff.tool nvim_difftool
+git config --global difftool.nvim_difftool.cmd 'nvim -c "packadd nvim.difftool" -c "DiffTool $LOCAL $REMOTE"'
